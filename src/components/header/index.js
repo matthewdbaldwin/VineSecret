ba5b603056a54eb698eedcc5dfd057d617f1492a
Matthew Baldwin <matt.d.baldwin@gmail.com>
2020-01-26 21:29:13 -0800

    began structure of site

---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Nav from '..nav';
+
+export default props => {
+    return (
+        <div className="row">
+            <header>
+                <div className="top-image">
+
+                </div>
+                <Nav/>
+                <div className="main-icon"></div>
+                <h2>The Vines Are Our Secret!</h2>
+            </header>
+
+        </div>
+    )
+}
---
+++ src/components/nav/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './nav.scss';
+
+export default props => {
+    return (
+        <nav className="navbar float-right">
+            <ul className="main-nav">
+                <li className="nav-item nav-link active">
+                    <Link className={`menuList link `} to="/">HOME</Link>
+                </li>
+                <li className="nav-item nav-link">
+                    <Link className={`menuList link `} to="/products">PRODUCTS</Link>
+                </li>
+                <li className="nav-item nav-link">
+                    <Link className={`menuList link `} to="/contact">CONTACT</Link>
+                </li>
+                <li className="nav-item nav-link">
+                    <Link className={`menuList link `} to="/about">ABOUT US</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
