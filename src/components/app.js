139d30d3bde782a0417736ca52378bd1d747e2bd
Matthew Baldwin <matt.d.baldwin@gmail.com>
2020-01-26 21:30:49 -0800

    began structure of site

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import '../assets/css/app.scss';
-import logo from '../assets/images/logo.svg';
+import About from './about';
+import Contact from './contact';
+import Footer from './footer';
+import Header from './header';
+import Home from './home';
+import Products from './products';
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome To React</h1>
+    <div className="app">
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Header />
+                    <Route path="/" exact component={Home} />
+                    <Route path="/products" exact component={Products} />
+                    <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} />
+                    <Footer />
+                </div>
+            </div>
         </div>
     </div>
 );
