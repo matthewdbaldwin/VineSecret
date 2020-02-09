5c835f21374e8511acd5e4507aacb1c193aea83a
mantismb <matt.d.baldwin@gmail.com>
2020-02-08 17:22:59 -0800

    Created header and footer

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,10 +4,15 @@ import Nav from '../nav';
 
 export default props => {
     return (
-        <header className="row">
-                <div className="col-2 main-icon"></div>
-                <div className="col-8"><Nav/></div>
-                <h2>The Vines Are Our Secret!</h2>
-        </header>
+        <div className="row">
+            <header >
+                <div className="row">
+                    <div className="col-4 main-icon"></div>
+                    <div className="col-8 main-nav"><Nav/></div>
+                    <div className="col-12">
+                    <h2>The Vines Are Our Secret!</h2> </div>
+                </div>
+            </header>
+        </div>
     )
 }
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,21 +3,21 @@ import React from 'react';
 
 export default props => {
     return (
-            <div className="row">
-                <div className="div1">
-                    <main className="col">
-                        <button class="button">
-                            Click here<br>
-                            </br>for our wine selection!
-                        </button>  
-                    </main>  
+        <div className="row">
+            <div className="welcome col-12">
+                <h1>Welcome to VineSecret!</h1>
+                <main className="cta col-4 ">
+                    <button className="button">
+                        Click here<br>
+                        </br>for our wine selection!
+                    </button>  
+                </main>  
             </div>
-                <div className="newsletter">
-                    <button class='btn-warning'>
-                        Great deals online in our Newsletter!
-                    </button>
-        
-                </div>
+            <div className="col-12 newsletter">
+                <button className='btn-warning'>
+                    Great deals online in our Newsletter!
+                </button>
+            </div>
         </div>
     );
 }
