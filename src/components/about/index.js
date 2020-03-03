b48e346cd508820b3c0a39b2ae862da8a4917dfb
Matthew Baldwin <matt.d.baldwin@gmail.com>
2020-03-03 12:10:46 -0800

    Added about page

---
+++ src/components/about/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './about.scss';
+
+export default props => {
+    return (
+        <div className="row"> 
+            <div className="col-12">
+                <main>
+                    <div className="textbox">
+                        <h2>About Us</h2>
+                        <p>
+                            We are adventurous vintners in the foothills of Orange County. Using a wide variety of vines from the California coast, we have perfected our wines inland.
+                        </p>
+                        <p>
+                            Please feel free to visit us in Orange County.
+                        </p>
+                    </div>
+                </main>
+            </div>
+        </div>
+    );
+}
