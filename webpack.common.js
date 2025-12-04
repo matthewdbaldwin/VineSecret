e236a5c9ae61c8d206a62ae18394661fec291768
Matthew Baldwin <47402880+matthewdbaldwin@users.noreply.github.com>
2025-12-04 13:12:20 -0800

    Update dependencies and dev server tooling

--- webpack.common.js
+++ webpack.common.js
@@ -37,15 +37,16 @@ module.exports = {
             },
             {
                 test: /\.(gif|png|jpe?g|svg)$/i,
-                use: ['file-loader?hash=sha512&digest=hex&name=assets/images/[hash].[ext]']
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[hash][ext][query]'
+                }
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        outputPath: 'assets/fonts/'
-                    }
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[hash][ext][query]'
                 }
             }
         ]
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const merge = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
@@ -10,12 +10,19 @@ module.exports = merge(common, {
         './index.js'
     ],
     mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                use: ['babel-loader']
-            }
+    devtool: 'source-map',
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    format: {
+                        comments: false
+                    }
+                },
+                extractComments: false
+            })
         ]
     },
     plugins: [
@@ -23,9 +30,6 @@ module.exports = merge(common, {
             'process.env': {
                 NODE_ENV: JSON.stringify('production')
             }
-        }),
-        new UglifyJsPlugin({
-            sourceMap: true
         })
     ]
 });
