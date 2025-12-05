a32b525dc19a9aa86b45b7e9c6717a2dc574ac71
Matthew Baldwin <47402880+matthewdbaldwin@users.noreply.github.com>
2025-12-05 11:26:07 -0800

    Restore html-webpack-plugin for index generation

--- dev_server.js
+++ dev_server.js
@@ -6,17 +6,18 @@ const npm_config = require('./package.json');
 const PORT = process.env.PORT || 3000;
 
 const compiler = webpack(config);
-const server = new WebpackDevServer({
+const server = new WebpackDevServer(compiler, {
     ...config.devServer,
-    static: {
-        directory: resolve(__dirname, 'dist')
-    },
-    proxy: npm_config.proxy
-}, compiler);
+    proxy: npm_config.proxy,
+    contentBase: resolve(__dirname, 'dist'),
+    host: '0.0.0.0'
+});
 
-server.start(PORT, '0.0.0.0').catch(err => {
-    console.log('\n\x1b[37m%s\x1b[33m%s\x1b[37m%s\x1b[36m%s\x1b[33m%s\x1b[0m\n\n', '========', ' REACT DEV SERVER LISTEN ERROR ', '========','\nYou probably already have a server running on PORT:', PORT);
+server.listen(PORT, '0.0.0.0', err => {
+    if (err) {
+        console.log('\n\x1b[37m%s\x1b[33m%s\x1b[37m%s\x1b[36m%s\x1b[33m%s\x1b[0m\n\n', '========', ' REACT DEV SERVER LISTEN ERROR ', '========','\nYou probably already have a server running on PORT:', PORT);
 
-    console.error(err);
-    server.stop();
+        console.error(err);
+        server.close();
+    }
 });
