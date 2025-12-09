57ab9a716a551a8be62d2a5e3066af6baed85cf4
Matthew Baldwin <47402880+matthewdbaldwin@users.noreply.github.com>
2025-12-09 09:42:18 -0800

    Use bundled HTML plugin to avoid missing dependency

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,39 +4,15 @@ const common = require('./webpack.common');
 const PORT = process.env.PORT || 3000;
 
 module.exports = merge(common, {
-<<<<<<< HEAD
-    entry: [
-        'core-js/stable',
-        'regenerator-runtime/runtime',
-        'webpack-dev-server/client?http://localhost:' + PORT,
-        './index.js'
-    ],
-    devtool: false,
-    mode: 'development',
-    plugins: [
-        new webpack.NamedModulesPlugin(),
-        new webpack.SourceMapDevToolPlugin({
-            filename: '[file].map',
-            moduleFilenameTemplate: 'webpack://[namespace]/[resource-path]?[hash]',
-            fallbackModuleFilenameTemplate: 'webpack://[namespace]/[resource-path]?[hash]',
-            hashFunction: 'sha256'
-        })
-    ]
-=======
     devtool: 'source-map',
     mode: 'development',
     devServer: {
         host: '0.0.0.0',
-        static: {
-            directory: resolve(__dirname, 'dist')
-        },
+        contentBase: resolve(__dirname, 'dist'),
         historyApiFallback: true,
         hot: false,
+        publicPath: '/',
         port: PORT,
-        allowedHosts: 'all',
-        client: {
-            logging: 'info'
-        }
+        disableHostCheck: true
     }
->>>>>>> 02fff9e5e4f4887cf4492e23c51863e75689be74
 });
