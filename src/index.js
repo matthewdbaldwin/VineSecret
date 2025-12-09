21d36461a0f46cbb72a08cee46ff81e2ee35a085
Matthew Baldwin <47402880+matthewdbaldwin@users.noreply.github.com>
2025-12-09 09:46:43 -0800

    Restore html-webpack-plugin for webpack builds

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
@@ -11,12 +11,25 @@ import thunk from './middleware/thunk';
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
 
-root.render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>
-);
+// Support both legacy ReactDOM.render (for older installs) and the React 18
+// createRoot API when available.
+if (typeof ReactDOM.createRoot === 'function') {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>,
+    );
+} else {
+    ReactDOM.render(
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>,
+        rootElement,
+    );
+}
